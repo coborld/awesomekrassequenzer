f74c570591ecae6c20ae557f96c23ec33e372f27
bplu4t2f <bplu4t2f@gmail.com>
2015-03-18 22:13:17 +0100

    Fixed the InvalidNoteValueException (runtime serialization parameters)

--- krassequenzer/krassequenzer/MusicModel/InvalidNoteValueException.cs
+++ krassequenzer/krassequenzer/MusicModel/InvalidNoteValueException.cs
@@ -8,6 +8,13 @@ namespace krassequenzer.MusicModel
 {
 	class InvalidNoteValueException : Exception
 	{
+		public InvalidNoteValueException()
+		{
+		}
 
+		public InvalidNoteValueException(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context)
+			: base(info, context)
+		{
+		}
 	}
 }
